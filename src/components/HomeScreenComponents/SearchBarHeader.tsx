22211c562b8790e2ebe398c7b63b5e2a22c3c36f
Neel <neelnarayanshetty@gmail.com>
2023-03-21 18:58:23 +0530

    updates

--- src/components/HomeScreenComponents/SearchBarHeader.tsx
+++ src/components/HomeScreenComponents/SearchBarHeader.tsx
@@ -1,16 +1,21 @@
-import {StyleSheet, View} from 'react-native';
+import {TouchableOpacity, StyleSheet, View} from 'react-native';
 import React from 'react';
 import SearchBar from '../SearchBar';
 import SearchScreen from '../../screens/SearchScreen';
 import ExploreTabs from '../../Navigator/ExploreTabs';
+import ProductScreen from '../../screens/ProductScreen';
+import {useNavigation} from '@react-navigation/native';
 
 const SearchBarHeader = () => {
+  const navigation: any = useNavigation();
   return (
     <View style={styles.root}>
-      <SearchBar
-        placeholder="Search Store"
-        navigateTo={(ExploreTabs.name, {screen: SearchScreen.name})}
-      />
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate(ExploreTabs.name, {screen: SearchScreen.name});
+        }}>
+        <SearchBar placeholder="Search Store" navigateTo={null} />
+      </TouchableOpacity>
     </View>
   );
 };
